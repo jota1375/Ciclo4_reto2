50a8358a5753c12cd7db660ff8e2889958cb75f2
jota1375 <87089846+jota1375@users.noreply.github.com>
2021-11-30 23:39:15 -0500

    Add files via upload
    cciclo4 reto2

---
+++ ciclo4/src/main/java/co/usa/edu/ciclo4/model/Producto.java
@@ -0,0 +1,24 @@
+package co.usa.edu.ciclo4.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "producto")
+public class Producto {
+	
+	@Id
+	private String id;
+	private String nombre;
+	private String categoria;
+	private String disponibilidad;
+	private String precio;
+	private String stock;
+		
+}
